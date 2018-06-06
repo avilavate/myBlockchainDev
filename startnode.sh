7ec464ca62f3f773b0aa6b6a3f4c9c3b1b18d4e4
Avil Avate <avilavate123@gmail.com>
2018-06-06 15:13:00 +0530

    Create startnode.sh
    Added startnode shell command script

---
+++ startnode.sh
@@ -0,0 +1,3 @@
+ ./geth.exe --networkid 4224 --mine --minerthreads 2 --datadir "./assn2" --
+nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock "2" --password .\password.sec --ipcpath "~/.ethereum/ge
+th.ipc"
